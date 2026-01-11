e69b5c76bf07a14acdd76f0d31f25392679569a9
ahmedmokdad780-sys <ahmedmokdad780@gmail.com>
2026-01-11 14:44:04 +0300

    change

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,7 +12,9 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'last_name' => $this->last_name,
             'phone' => $this->phone,
+            'account_type'   => $this->account_type,
             // التعديل هنا: استخدم اسم العمود الفعلي مع asset
-'personal_image' => $this->personal_image ? asset('storage/' . $this->personal_image) : null,        ];
+            'personal_image' => $this->personal_image ? asset('storage/' . $this->personal_image) : null,
+        ];
     }
 }
